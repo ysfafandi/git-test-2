3fdbecf0f4790a1ef6b426f4c7b7a5d715d86aa6
fandtype@gmail.com <fandtype@gmail.com>
2024-11-07 11:23:17 +0700

    menambah paragraph baru

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mencoba git remote</title>
+</head>
+<body>
+    <h1>Helo word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, amet beatae vitae et laboriosam debitis nemo, eligendi accusantium tempore nisi quo totam cumque possimus eaque velit doloribus rem. Sapiente, odit!</p>
+    
+</body>
+</html>
